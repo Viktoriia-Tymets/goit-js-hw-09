325565584a13ea46b1f6c2949f2faec25421c6f9
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-01-25 19:15:09 +0000

    +

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,44 +1,49 @@
 const formData = {
     email: '',
     message: '',
-}
-const feedbackFormEl = document.querySelector('.feedback-form');
+};
 
 const formDataFromLS = 'feedback-form-state';
-feedbackFormEl.addEventListener('input', (event) => {
-    formData[event.target.name] = event.target.value.trim();
-    localStorage.setItem(feedbackFormEl, JSON.stringify(formData));
-});
 
-window.addEventListener('load', () => {
-    const savedData = localStorage.getItem(feedbackFormEl);
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+feedbackFormEl.addEventListener('input', handleInput);
+feedbackFormEl.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(formDataFromLS, JSON.stringify(formData));
+};
+
+function completeForm() {
+    const savedData = localStorage.getItem(formDataFromLS);
+
     if (savedData) {
         const parsedData = JSON.parse(savedData);
-
-        Object.keys(parsedData).forEach((key) => {
-            form.elements[key].value = parsedData[key];
-            formData[key] = parsedData[key];
-        });
+        
+        formData.email = parsedData.email || "";
+        formData.message = parsedData.message || "";
+        feedbackFormEl.elements.email.value = formData.email;
+        feedbackFormEl.elements.message.value = formData.message;
     }
-});
+};
 
-feedbackFormEl.addEventListener('submit', (event) => {
+function handleSubmit(event) {
     event.preventDefault();
 
     if (!formData.email || !formData.message) {
         alert('Fill please all fields');
         return;
     }
+    
 console.log(formData);
 
-localStorage.removeItem(feedbackFormEl);
 formData.email = "";
 formData.message = "";
+localStorage.removeItem(formDataFromLS);
 feedbackFormEl.reset();
-})
 
-
-
+};
 
 
 
